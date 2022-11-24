f40773cea8bc0f36f9f0ecbe8247063f0914b0ac
e-meyer <edmundom@mx2.unisc.br>
2022-11-24 12:14:41 -0300

    feat: create file containing the colors pallete of the app

---
+++ lib/core/utils/constants/colors_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color aquaGreen = Color(0xFF00C1AF);
+const Color lightGrey = Color(0xFFF8F8F8);
+const Color darkGrey = Color(0xFF828282);
+const Color plainWhite = Color(0xFFFFFFFF);
+const Color plainBlack = Color(0xFF202021);
