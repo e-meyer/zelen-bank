04ba124a5957c0abe496c6b3c686ca266db1bcfb
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-24 17:30:42 -0300

    update: aplicar novo tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/theme/theme.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,9 +10,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Zelen Bank',
-      home: Scaffold(),
+      theme: CustomTheme.getTheme(),
+      home: const Scaffold(),
     );
   }
 }
