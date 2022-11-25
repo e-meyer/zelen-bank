f1531ab0cda89af4e3cdae1bea0dce9a4b459de3
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-25 11:46:37 -0300

    feat: adding Material Color color to constants

--- lib/core/utils/constants/colors_constants.dart
+++ lib/core/utils/constants/colors_constants.dart
@@ -5,3 +5,18 @@ const Color kLightGrey = Color(0xFFF8F8F8);
 const Color kDarkGrey = Color(0xFF828282);
 const Color kPlainWhite = Color(0xFFFFFFFF);
 const Color kPlainBlack = Color(0xFF202021);
+const MaterialColor white = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
