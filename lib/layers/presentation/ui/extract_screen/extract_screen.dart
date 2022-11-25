130d8d4e50fbc23ad90737ee5dbc8f23f020ddf9
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-25 11:10:43 -0300

    feat: removing debug and surplus packages

--- lib/layers/presentation/ui/extract_screen/extract_screen.dart
+++ lib/layers/presentation/ui/extract_screen/extract_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ExtractScreen extends StatelessWidget {
   const ExtractScreen({super.key});
