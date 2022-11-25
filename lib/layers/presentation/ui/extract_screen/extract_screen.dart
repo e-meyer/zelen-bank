560282947cedef084176fc3d8e9e5fb95f3c9844
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-25 12:09:49 -0300

    feat: turning status bar transparent

--- lib/layers/presentation/ui/extract_screen/extract_screen.dart
+++ lib/layers/presentation/ui/extract_screen/extract_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ExtractScreen extends StatelessWidget {
   const ExtractScreen({super.key});
@@ -7,10 +8,18 @@ class ExtractScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
         title: const Text('Extrato'),
         centerTitle: true,
         elevation: 0.0,
       ),
+      body: Column(children: [
+        Container(
+          height: MediaQuery.of(context).size.height * .135,
+          color: Theme.of(context).backgroundColor,
+        ),
+      ]),
     );
   }
 }
