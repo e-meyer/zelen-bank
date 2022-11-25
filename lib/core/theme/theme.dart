31608ad4990f036b1fc1d5142fd5595259ff833c
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-25 11:47:20 -0300

    refactor: custom theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:zelenbank/core/utils/constants/colors_constants.dart';
 
+enum Tema {
+  lightTema,
+}
+
 class CustomTheme {
-  static ThemeData? getTheme() {
-    return ThemeData(
+  static final customtheme = {
+    Tema.lightTema: ThemeData(
+      brightness: Brightness.light,
       primaryColor: kAquaGreen,
       backgroundColor: kLightGrey,
+      primarySwatch: white,
       textTheme: const TextTheme(
         headline1: TextStyle(color: kPlainBlack),
         headline2: TextStyle(color: kPlainBlack),
         bodyText2: TextStyle(color: kPlainBlack),
         subtitle1: TextStyle(color: kPlainBlack),
       ),
-    );
-  }
+    ),
+  };
 }
