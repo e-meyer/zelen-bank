5865e65962f599944ef83641614bd5094486cf13
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-25 17:35:06 -0300

    Refactor: extract compose text components

--- lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
+++ lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:zelenbank/layers/domain/entities/transaction_entity.dart';
 import 'package:zelenbank/layers/presentation/ui/common/back_button.dart';
 import 'package:zelenbank/layers/presentation/controllers/transaction_controller.dart';
+import 'package:zelenbank/layers/presentation/ui/common/back_button.dart';
 import '../../../../../core/injector/injector.dart';
+import '../components/BankName.dart';
+import '../components/TransactionType.dart';
 
 class TransactionDetails extends StatelessWidget {
   final TransactionController _transactionController =
       serviceLocator.get<TransactionController>();
   final String id;
   TransactionDetails(this.id, {Key? key}) : super(key: key);
-
   // TODO: CORRIGIR ESPAÇAMENTOS APÓS CONCLUIR PARTE DE DADOS
   @override
   Widget build(BuildContext context) {
@@ -18,33 +20,38 @@ class TransactionDetails extends StatelessWidget {
           future: _transactionController.getById(id),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Column(children: [
-                Text('Comprovante'),
-                Divider(),
-                Column(children: [
-                  Text('Tipo de Movimentação'),
-                  Text(snapshot.data!.transactionType),
-                  Text('Valor'),
-                  Text(snapshot.data!.amount.toString()),
-                  Text(_transactionController.isReceived(snapshot.data!)
-                      ? 'Recebedor'
-                      : 'Pagador'),
-                  Text(snapshot.data!.targetName),
-                  snapshot.data!.bankName != null
-                      ? Text('Instituição bancária')
-                      : Container(),
-                  snapshot.data!.bankName != null
-                      ? Text(snapshot.data!.bankName!)
-                      : Container(),
-                  Text('Data/Hora'),
-                  Text(snapshot.data!.createdAt.toString()),
-                  Text('Autenticação'),
-                  snapshot.data!.authentication != null
-                      ? Text(snapshot.data!.authentication!)
-                      : Container()
-                ]),
-                ElevatedButton(onPressed: () {}, child: Text('Compartilhar'))
-              ]);
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(
+                            right: MediaQuery.of(context).size.width * .8),
+                        child: CustomBackButton()),
+                    Text('Comprovante'),
+                    Divider(),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Tipo de Movimentação'),
+                          Text(snapshot.data!.transactionType),
+                          Text('Valor'),
+                          Text(snapshot.data!.amount.toString()),
+                          TransactionType(
+                            transactionController: _transactionController,
+                            data: snapshot.data!,
+                          ),
+                          Text(snapshot.data!.targetName),
+                          BankName(snapshot.data!.bankName),
+                          Text('Data/Hora'),
+                          Text(snapshot.data!.createdAt.toString()),
+                          Text('Autenticação'),
+                          snapshot.data!.authentication != null
+                              ? Text(snapshot.data!.authentication!)
+                              : Container()
+                        ]),
+                    ElevatedButton(
+                        onPressed: () {}, child: Text('Compartilhar'))
+                  ]);
             } else {
               return Center(
                 child: CircularProgressIndicator(),
