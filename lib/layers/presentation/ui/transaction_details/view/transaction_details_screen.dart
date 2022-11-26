8c3e58ea1a2e0eb31abe94ec05d09149ec123452
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-25 21:43:56 -0300

    feat: add share button functionality

--- lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
+++ lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zelenbank/core/share_image/share_image.dart';
 import 'package:zelenbank/layers/domain/entities/transaction_entity.dart';
 import 'package:zelenbank/layers/presentation/ui/common/back_button.dart';
 import 'package:zelenbank/layers/presentation/controllers/transaction_controller.dart';
@@ -14,7 +15,9 @@ class TransactionDetails extends StatelessWidget {
   final TransactionController _transactionController =
       serviceLocator.get<TransactionController>();
   final String id;
+  GlobalKey previewContainer = new GlobalKey();
   TransactionDetails(this.id, {Key? key}) : super(key: key);
+
   // TODO: CORRIGIR ESPAÇAMENTOS APÓS CONCLUIR PARTE DE DADOS
   @override
   Widget build(BuildContext context) {
@@ -31,30 +34,33 @@ class TransactionDetails extends StatelessWidget {
                         padding: EdgeInsets.only(
                             right: MediaQuery.of(context).size.width * .8),
                         child: CustomBackButton()),
-                    Text('Comprovante'),
                     Divider(),
-                    Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('Tipo de Movimentação'),
-                          Text(constants.Constants[data.transactionType]!),
-                          Text('Valor'),
-                          Text('R\$${data.amount.toString()}'),
-                          TransactionType(
-                            transactionController: _transactionController,
-                            data: data,
-                          ),
-                          Text(data.targetName),
-                          BankName(data.bankName),
-                          Text('Data/Hora'),
-                          Text(data.createdAt.toString()),
-                          Authentication(),
-                          data.authentication != null
-                              ? Text(data.authentication!)
-                              : Container()
-                        ]),
+                    RepaintBoundary(
+                        key: previewContainer,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('Comprovante'),
+                              Text('Tipo de Movimentação'),
+                              Text(constants.Constants[data.transactionType]!),
+                              Text('Valor'),
+                              Text('R\$${data.amount.toString()}'),
+                              TransactionType(
+                                transactionController: _transactionController,
+                                data: data,
+                              ),
+                              Text(data.targetName),
+                              BankName(data.bankName),
+                              Text('Data/Hora'),
+                              Text(data.createdAt.toString()),
+                              Authentication(),
+                              data.authentication != null
+                                  ? Text(data.authentication!)
+                                  : Container()
+                            ])),
                     ElevatedButton(
-                        onPressed: () {}, child: Text('Compartilhar'))
+                        onPressed: () => shareImage(previewContainer),
+                        child: Text('Compartilhar'))
                   ]);
             } else {
               return Center(
