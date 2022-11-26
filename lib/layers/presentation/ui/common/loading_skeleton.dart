ebc8d968a983737af1cd50d1366ef555c3c1a5c4
e-meyer <edmundom@mx2.unisc.br>
2022-11-26 00:00:56 -0300

    refactor: loading_skeleton is now a stateless widget

--- lib/layers/presentation/ui/common/loading_skeleton.dart
+++ lib/layers/presentation/ui/common/loading_skeleton.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:zelenbank/core/utils/constants/colors_constants.dart';
 
-class LoadingSkeleton extends StatefulWidget {
+class LoadingSkeleton extends StatelessWidget {
   const LoadingSkeleton({
     Key? key,
     required this.height,
@@ -10,25 +10,19 @@ class LoadingSkeleton extends StatefulWidget {
     required this.radius,
   }) : super(key: key);
 
-  final double? height, width;
-  final double radius;
+  final double height, width, radius;
 
-  @override
-  State<LoadingSkeleton> createState() => _LoadingSkeletonState();
-}
-
-class _LoadingSkeletonState extends State<LoadingSkeleton> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(widget.radius),
+      borderRadius: BorderRadius.circular(radius),
       child: Shimmer.fromColors(
         period: const Duration(milliseconds: 800),
         baseColor: kDarkGrey,
         highlightColor: kLightGrey,
         child: Container(
-          height: widget.height,
-          width: widget.width,
+          height: height,
+          width: width,
           decoration: BoxDecoration(
             color: kLightGrey.withOpacity(0.25),
           ),
