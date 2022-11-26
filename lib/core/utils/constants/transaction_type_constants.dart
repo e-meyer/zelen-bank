f60a848bf5f095d551f0d882a32f836176661906
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-25 23:29:31 -0300

    Refactor: delegate constant handling
    Delegate transfer response field name handling to constant file

--- lib/core/utils/constants/transaction_type_constants.dart
+++ lib/core/utils/constants/transaction_type_constants.dart
@@ -1,6 +1,8 @@
-Map<String, String> Constants = const {
+Map<String, String> transactionTypeMap = const {
   'TRANSFEROUT': 'Transferência enviada',
   'TRANSFERIN': 'Transferência recebida',
   'PIXCASHOUT': 'Transferência PIX enviada',
   'PIXCASHIN': 'Tranferência PIX recebida'
 };
+
+Set transferInTypes = {'TRANFERIN', 'PIXCASHIN'};
