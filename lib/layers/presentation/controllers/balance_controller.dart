a31c6b42274f5f16e36dbede540afa0b06d1867c
e-meyer <edmundom@mx2.unisc.br>
2022-11-26 22:14:32 -0300

    feat: create function to get the current state of the balance visibility flag and created usecase variable to be used

--- lib/layers/presentation/controllers/balance_controller.dart
+++ lib/layers/presentation/controllers/balance_controller.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:zelenbank/layers/domain/usecases/change_balance_visibility/change_balance_visibility_usecase.dart';
+import 'package:zelenbank/layers/domain/usecases/get_balance_visibility/get_balance_visibility_usecase.dart';
 
 import '../../domain/usecases/get_current_balance_usecase/get_current_balance_usecase.dart';
 
 class BalanceController extends ChangeNotifier {
   final GetCurrentBalanceUsecase _getCurrentBalanceUsecase;
+  final ChangeBalanceVisibilityUsecase _changeBalanceVisibilityUsecase;
+  final GetBalanceVisibilityUsecase _getBalanceVisibilityUsecase;
   double _balance = 0;
+  bool isBalanceVisible = true;
 
   BalanceController(
     this._getCurrentBalanceUsecase,
+    this._changeBalanceVisibilityUsecase,
+    this._getBalanceVisibilityUsecase,
   );
 
   double get balance => _balance;
 
   void fetchBalance() async {
     _balance = await _getCurrentBalanceUsecase();
+    notifyListeners();
+  }
+
+  void getBalanceVisibility() async {
+    isBalanceVisible = await _getBalanceVisibilityUsecase();
+    notifyListeners();
+  }
+
+  void changeBalanceVisibility() async {
+    isBalanceVisible = await _changeBalanceVisibilityUsecase(isBalanceVisible);
     notifyListeners();
   }
 }
