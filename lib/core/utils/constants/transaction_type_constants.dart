73663e5c061cef8ead1036ad89cfbed120316803
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-28 02:31:05 -0300

    refactor: remove unnused constant

--- lib/core/utils/constants/transaction_type_constants.dart
+++ lib/core/utils/constants/transaction_type_constants.dart
@@ -1,9 +1,1 @@
-Map<String, String> transactionTypeMap = const {
-  'TRANSFEROUT': 'Transferência enviada',
-  'TRANSFERIN': 'Transferência recebida',
-  'PIXCASHOUT': 'Transferência PIX enviada',
-  'PIXCASHIN': 'Tranferência PIX recebida',
-  'BANKSLIPCASHIN': 'Depósito via boleto',
-};
-
 Set transferInTypes = {'TRANSFERIN', 'PIXCASHIN'};
