4f749d22365c2b70ba1d845c5f338d006b0ee03b
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-27 23:00:35 -0300

    feat: full button tests

--- test/layers/presentation/ui/transaction_datails/components/share_button_widget_test.dart
+++ test/layers/presentation/ui/transaction_datails/components/share_button_widget_test.dart
@@ -3,14 +3,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zelenbank/layers/presentation/ui/transaction_details/components/custon_button_widget.dart';
 
 void main() {
-  test('test button click', () {
-    int count = 0;
-    GlobalKey previewContainer = GlobalKey();
-    CustonButtonWidget custonButtonWidget = CustonButtonWidget(
-      label: 'test',
-      onTap: () => count++,
-      previewContainer: previewContainer,
-    );
+  int count = 0;
+  GlobalKey<State<StatefulWidget>> previewContainer =
+      GlobalKey<State<StatefulWidget>>();
+  CustonButtonWidget custonButtonWidget = CustonButtonWidget(
+    label: 'test',
+    onTap: () => count++,
+    previewContainer: previewContainer,
+  );
+  test('test button label', () {
     expect(custonButtonWidget.label, 'test');
+  });
+
+  test('test button click', () {
+    expect(count, 0);
+    custonButtonWidget.onTap!();
+    expect(count, 1);
+    custonButtonWidget.onTap!();
+    expect(count, 2);
+    custonButtonWidget.onTap!();
+    expect(count, 3);
+  });
+
+  test('test button preview', () {
+    expect(custonButtonWidget.previewContainer, previewContainer);
   });
 }
