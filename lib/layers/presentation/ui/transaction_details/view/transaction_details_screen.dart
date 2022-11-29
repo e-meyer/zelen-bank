5ff574f0a87ba4df2f5d3cec1fb9c70ee29aa1db
e-meyer <edmundom@mx2.unisc.br>
2022-11-29 09:58:34 -0300

    refactor: added detailedTransaction variable in controller to be able to handle failures

--- lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
+++ lib/layers/presentation/ui/transaction_details/view/transaction_details_screen.dart
@@ -12,128 +12,121 @@ import '../components/transaction_details_field_widget.dart';
 import '../components/transaction_details_loading_widget.dart';
 
 // ignore: must_be_immutable
-class TransactionDetails extends StatelessWidget {
+class TransactionDetails extends StatefulWidget {
   final String id;
 
   TransactionDetails(this.id, {Key? key}) : super(key: key);
 
+  @override
+  State<TransactionDetails> createState() => _TransactionDetailsState();
+}
+
+class _TransactionDetailsState extends State<TransactionDetails> {
   final TransactionController _transactionController =
       serviceLocator.get<TransactionController>();
 
   GlobalKey previewContainer = GlobalKey();
 
   @override
+  void initState() {
+    super.initState();
+    _transactionController.getTransactionById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMethod(titulo: '', leading: const CustomBackButton()),
-      body: FutureBuilder<TransactionEntity>(
-        future: _transactionController.getTransactionById(id),
+      body: AnimatedBuilder(
+        animation: _transactionController,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return Padding(
-                  padding: const EdgeInsets.only(top: 45),
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return const TransactionDetailsLoadingWidget();
-                    },
-                    itemCount: 8,
-                  ),
-                );
-              case ConnectionState.active:
-                return Container();
-              case ConnectionState.none:
-                return Container();
-              case ConnectionState.done:
-                TransactionEntity data = snapshot.data!;
-                return Column(
+          TransactionEntity? data = _transactionController.detailedTransaction;
+          if (data != null) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RepaintBoundary(
-                          key: previewContainer,
-                          child: Container(
-                            color: kPlainWhite,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 15),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    'Comprovante',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const Divider(
-                                    thickness: 2.2,
-                                    height: 25,
-                                  ),
-                                  TransactionDetailsFieldWidget(
-                                    fieldTitle: 'Tipo de movimentação',
-                                    fieldDescription: data.description,
-                                  ),
-                                  TransactionDetailsFieldWidget(
-                                    fieldTitle: 'Valor',
-                                    fieldDescription:
-                                        'R\$ ${transactionFormattedValue(data.amount)}',
-                                  ),
-                                  TransactionDetailsFieldWidget(
-                                    fieldTitle:
-                                        _transactionController.isReceived(data)
-                                            ? 'Pagador'
-                                            : 'Recebedor',
-                                    fieldDescription: data.targetName,
-                                  ),
-                                  data.bankName != null
-                                      ? TransactionDetailsFieldWidget(
-                                          fieldTitle: 'Instituição bancária',
-                                          fieldDescription: data.bankName!,
-                                        )
-                                      : Container(),
-                                  TransactionDetailsFieldWidget(
-                                    fieldTitle: 'Data/Hora',
-                                    fieldDescription:
-                                        DateFormat('d/MM/yyyy - HH:mm:ss')
-                                            .format(data.createdAt),
-                                  ),
-                                  TransactionDetailsFieldWidget(
-                                    fieldTitle: 'Autenticação',
-                                    fieldDescription: data.authentication!,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
+                    RepaintBoundary(
+                      key: previewContainer,
+                      child: Container(
+                        color: kPlainWhite,
                         child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 8,
-                          ),
-                          child: CustomButtonWidget(
-                            label: 'Compartilhar',
-                            previewContainer: previewContainer,
-                            onTap: () {
-                              return shareImage(previewContainer);
-                            },
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Comprovante',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const Divider(
+                                thickness: 2.2,
+                                height: 25,
+                              ),
+                              TransactionDetailsFieldWidget(
+                                fieldTitle: 'Tipo de movimentação',
+                                fieldDescription: data.description,
+                              ),
+                              TransactionDetailsFieldWidget(
+                                fieldTitle: 'Valor',
+                                fieldDescription:
+                                    'R\$ ${transactionFormattedValue(data.amount)}',
+                              ),
+                              TransactionDetailsFieldWidget(
+                                fieldTitle:
+                                    _transactionController.isReceived(data)
+                                        ? 'Pagador'
+                                        : 'Recebedor',
+                                fieldDescription: data.targetName,
+                              ),
+                              data.bankName != null
+                                  ? TransactionDetailsFieldWidget(
+                                      fieldTitle: 'Instituição bancária',
+                                      fieldDescription: data.bankName!,
+                                    )
+                                  : Container(),
+                              TransactionDetailsFieldWidget(
+                                fieldTitle: 'Data/Hora',
+                                fieldDescription:
+                                    DateFormat('d/MM/yyyy - HH:mm:ss')
+                                        .format(data.createdAt),
+                              ),
+                              TransactionDetailsFieldWidget(
+                                fieldTitle: 'Autenticação',
+                                fieldDescription: data.authentication!,
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ),
                   ],
-                );
-            }
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 8,
+                      ),
+                      child: CustomButtonWidget(
+                        label: 'Compartilhar',
+                        previewContainer: previewContainer,
+                        onTap: () {
+                          return shareImage(previewContainer);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
           }
           return Padding(
             padding: const EdgeInsets.only(top: 45),
