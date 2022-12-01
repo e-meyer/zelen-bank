0065e03a7dc991f1c7cc290e73f007d08f2f3897
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-01 16:30:13 -0300

    feat: test generic custom button rendering

--- test/layers/presentation/ui/common/share_button_widget_test.dart
+++ test/layers/presentation/ui/common/share_button_widget_test.dart
@@ -30,4 +30,27 @@ void main() {
     await tester.pumpAndSettle();
     expect(count, 1);
   });
+  testWidgets('Test widget rendering', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MediaQuery(
+        data: MediaQueryData(),
+        child: MaterialApp(
+          home: Scaffold(
+            body: CustomButtonWidget(
+              label: 'test',
+              previewContainer: previewContainer,
+              onTap: () {},
+            ),
+          ),
+        ),
+      ),
+    );
+    final BuildContext context = tester.element(find.byType(MaterialApp));
+
+    Finder theme = find.byType(Theme);
+    expect(theme, findsOneWidget, reason: 'No theme was found');
+
+    expect(MediaQuery.of(context).size.height, isA<double>(),
+        reason: 'Screen size is a not a valid number');
+  });
 }
