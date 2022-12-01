6986e4c6441176621543045ebef01e966ac2b84b
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-01 17:08:18 -0300

    feat: test transaction details field rendering

--- test/layers/presentation/ui/transaction_datails/components/transaction_details_field_widget_test.dart
+++ test/layers/presentation/ui/transaction_datails/components/transaction_details_field_widget_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zelenbank/layers/presentation/ui/transaction_details/components/transaction_details_field_widget.dart';
 
@@ -7,6 +9,38 @@ void main() {
     fieldTitle: 'fieldTitle',
     fieldDescription: 'fieldDescription',
   );
+
+  testWidgets('Widget renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(),
+      child: MaterialApp(
+        home: Scaffold(
+            body: const TransactionDetailsFieldWidget(
+          fieldTitle: 'fieldTitle',
+          fieldDescription: 'fieldDescription',
+        )),
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blue,
+          textTheme: TextTheme(
+            button: TextStyle(
+              fontSize: 16,
+              color: Colors.red,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    ));
+
+    final BuildContext context = tester.element(find.byType(MaterialApp));
+
+    expect(MediaQuery.of(context).size.height, isA<double>(),
+        reason: 'Screen size is a not a valid number');
+
+    Finder texts = find.byType(Text);
+    expect(texts, findsNWidgets(2));
+  });
 
   group('test transaction details widget', () {
     test('test transaction fieldTitle', () {
