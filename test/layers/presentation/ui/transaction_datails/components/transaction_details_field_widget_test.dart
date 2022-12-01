690410be1efa53b3c9a863df5dded47d20605c2a
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-01 17:09:03 -0300

    refactor: remove redundant tests

--- test/layers/presentation/ui/transaction_datails/components/transaction_details_field_widget_test.dart
+++ test/layers/presentation/ui/transaction_datails/components/transaction_details_field_widget_test.dart
@@ -10,48 +10,40 @@ void main() {
     fieldDescription: 'fieldDescription',
   );
 
-  testWidgets('Widget renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(MediaQuery(
-      data: MediaQueryData(),
-      child: MaterialApp(
-        home: Scaffold(
-            body: const TransactionDetailsFieldWidget(
-          fieldTitle: 'fieldTitle',
-          fieldDescription: 'fieldDescription',
-        )),
-        theme: ThemeData(
-          primaryColor: Colors.black,
-          scaffoldBackgroundColor: Colors.blue,
-          textTheme: TextTheme(
-            button: TextStyle(
-              fontSize: 16,
-              color: Colors.red,
-              fontWeight: FontWeight.w500,
+  testWidgets(
+    'Widget renders correctly',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MediaQuery(
+          data: MediaQueryData(),
+          child: MaterialApp(
+            home: Scaffold(
+                body: const TransactionDetailsFieldWidget(
+              fieldTitle: 'fieldTitle',
+              fieldDescription: 'fieldDescription',
+            )),
+            theme: ThemeData(
+              primaryColor: Colors.black,
+              scaffoldBackgroundColor: Colors.blue,
+              textTheme: TextTheme(
+                button: TextStyle(
+                  fontSize: 16,
+                  color: Colors.red,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
           ),
         ),
-      ),
-    ));
+      );
 
-    final BuildContext context = tester.element(find.byType(MaterialApp));
+      final BuildContext context = tester.element(find.byType(MaterialApp));
 
-    expect(MediaQuery.of(context).size.height, isA<double>(),
-        reason: 'Screen size is a not a valid number');
+      expect(MediaQuery.of(context).size.height, isA<double>(),
+          reason: 'Screen size is a not a valid number');
 
-    Finder texts = find.byType(Text);
-    expect(texts, findsNWidgets(2));
-  });
-
-  group('test transaction details widget', () {
-    test('test transaction fieldTitle', () {
-      expect(transactionDetailsFieldWidget.fieldTitle, 'fieldTitle');
-    });
-    test('test transaction fieldDescription', () {
-      expect(
-          transactionDetailsFieldWidget.fieldDescription, 'fieldDescription');
-    });
-    test('test field Title is not null', () {
-      expect(transactionDetailsFieldWidget.fieldTitle, isNotNull);
-    });
-  });
+      Finder texts = find.byType(Text);
+      expect(texts, findsNWidgets(2));
+    },
+  );
 }
