c2d7a0bb56e7be2cadb4be26e334a208a91740ae
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-01 04:50:47 -0300

    feat: create back button test

---
+++ test/layers/presentation/ui/common/back_button_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zelenbank/layers/presentation/ui/common/back_button.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+class FakeRoute extends Fake implements Route {}
+
+void main() {
+  setUpAll(() {
+    registerFallbackValue(FakeRoute());
+  });
+
+  testWidgets('Button is present and triggers navigation after tapped',
+      (WidgetTester tester) async {
+    final mockObserver = MockNavigatorObserver();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(body: CustomBackButton()),
+        navigatorObservers: [mockObserver],
+      ),
+    );
+
+    expect(find.byType(IconButton), findsOneWidget);
+    await tester.tap(find.byType(IconButton));
+    await tester.pumpAndSettle();
+
+    verify(() => mockObserver.didPop(any(), any()));
+    await tester.pumpAndSettle();
+  });
+}
