c254a0431b1a16c8c772ff04b387c0cc9e39e9a0
e-meyer <edmundom@mx2.unisc.br>
2022-12-02 00:10:06 -0300

    feat: include newly created functionalities in get_it

--- lib/authentication/presentation/controllers/auth_controller.dart
+++ lib/authentication/presentation/controllers/auth_controller.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:zelenbank/authentication/domain/usecases/check_user_auth_status_usecase.dart';
+import 'package:zelenbank/authentication/domain/usecases/check_user_auth_status/check_user_auth_status_usecase.dart';
+import 'package:zelenbank/authentication/domain/usecases/sign_in_with_google/sign_in_with_google_usecase.dart';
+import 'package:zelenbank/authentication/domain/usecases/sign_out/sign_out_google_usecase.dart';
 
 enum LoginState { idle, loading, success, error }
 
 class AuthController extends ChangeNotifier {
   final CheckUserAuthStatusUsecase _checkUserAuthStatusUsecase;
+  final SignInWithGoogleUsecase _signInWithGoogleUsecase;
+  final SignOutGoogleUsecase _signOutGoogleUsecase;
+  var isLogged = false;
   var loginState = LoginState.idle;
 
-  AuthController(this._checkUserAuthStatusUsecase);
+  AuthController(
+    this._checkUserAuthStatusUsecase,
+    this._signInWithGoogleUsecase,
+    this._signOutGoogleUsecase,
+  );
 
   isUserLoggedIn() {
-    return _checkUserAuthStatusUsecase();
-    // _result.fold(
-    //   (l) {
-    //     loginState = LoginState.error;
-    //     // return l;
-    //   },
-    //   (r) {
-    //     return r;
-    //   },
-    // );
+    isLogged = _checkUserAuthStatusUsecase();
+    notifyListeners();
+  }
+
+  signInWithGoogle() async {
+    loginState = LoginState.idle;
+    notifyListeners();
+    final _result = await _signInWithGoogleUsecase();
+
+    _result.fold((l) {
+      loginState = LoginState.error;
+    }, (r) {
+      loginState = LoginState.success;
+    });
+    notifyListeners();
+  }
+
+  signOutGoogle() async {
+    await _signOutGoogleUsecase();
   }
 }
