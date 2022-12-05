b061f17cdcd71aed9ffbb410d17f1f6fd73d2c08
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-05 03:54:19 -0300

    feat: add new color constant

--- lib/core/utils/constants/colors_constants.dart
+++ lib/core/utils/constants/colors_constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color kAquaGreen = Color(0xFF00C1AF);
+const Color kDarkGreen = Color(0xFF008073);
 const Color kLightGrey = Color(0xFFF8F8F8);
 const Color kDarkGrey = Color(0xFF828282);
 const Color kPlainWhite = Color(0xFFFFFFFF);
