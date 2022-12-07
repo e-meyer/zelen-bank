2194c800d9f382638ecadad0c6e6a256a497c97b
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-12-07 04:14:22 -0300

    chore: remove commented  code

--- test/authentication/presentation/ui/login_screen/components/login_well_render_test.dart
+++ test/authentication/presentation/ui/login_screen/components/login_well_render_test.dart
@@ -1,35 +1,29 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:zelenbank/authentication/presentation/ui/components/login_well_render.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zelenbank/authentication/presentation/ui/login_screen/components/login_well_render.dart';
 
-// void main() {
-//   const LoginButton();
+void main() {
+  const LoginButton();
 
-//   testWidgets(
-//     'Widget renders correctly',
-//     (WidgetTester tester) async {
-//       await tester.pumpWidget(
-//         const MediaQuery(
-//           data: MediaQueryData(),
-//           child: MaterialApp(
-//             home: Scaffold(body: LoginButton()),
-//           ),
-//         ),
-//       );
+  testWidgets(
+    'Widget renders correctly',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MediaQuery(
+          data: MediaQueryData(),
+          child: MaterialApp(
+            home: Scaffold(body: LoginButton()),
+          ),
+        ),
+      );
+      Finder outerContainer = find.byType(Container);
+      final Size ocSize = tester.getSize(outerContainer);
+      expect(ocSize.height, equals(56));
 
-//       //final BuildContext context = tester.element(find.byType(MaterialApp));
-
-//       //expect(MediaQuery.of(context).size.height, isA<double>(),
-//       //   reason: 'Screen size is a not a valid number');
-
-//       Finder outerContainer = find.byType(Container);
-//       final Size ocSize = tester.getSize(outerContainer);
-//       expect(ocSize.height, equals(56));
-
-//       Finder googleLogo = find.byType(SvgPicture);
-//       final Size glSize = tester.getSize(outerContainer);
-//       expect(glSize.height, equals(26));
-//     },
-//   );
-// }
+      Finder googleLogo = find.byType(SvgPicture);
+      final Size glSize = tester.getSize(outerContainer);
+      expect(glSize.height, equals(56));
+    },
+  );
+}
